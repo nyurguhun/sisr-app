0683db638ce82ffd6da82d7ac3735f5181bec22c
InaChemerko <innachemerko@gmail.com>
2019-09-26 21:52:21 -0700

    Added file upload

--- app.js
+++ app.js
@@ -32,7 +32,46 @@ dropZone.addEventListener('drop', function(e) {
 
             reader.readAsDataURL(file); // start reading the file data.
         } else {
-            alert("Please, choose img");
+            alert("Sorry! We support jpg format only!");
         }
     }
 });
+
+
+function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+
+    // Loop through the FileList and render image files as thumbnails.
+    for (var i = 0, f; f = files[i]; i++) {
+
+      // Only process image files.
+      if (!f.type.match('image.*')) {
+        continue;
+      }
+
+      var reader = new FileReader();
+
+      // Closure to capture the file information.
+      reader.onload = (function(theFile) {
+        return function(e) {
+          // Render thumbnail.
+          var newimg = document.createElement('img');
+                newimg.src= e.target.result;
+                var oldimg = document.getElementById('img');
+                oldimg.replaceWith(newimg);
+                newimg.setAttribute("id", "img");
+
+
+          /*var span = document.createElement('span');
+          span.innerHTML = ['<img class="thumb" src="', e.target.result,
+                            '" title="', escape(theFile.name), '"/>'].join('');
+          document.getElementById('list').insertBefore(span, null); */
+        };
+      })(f);
+
+      // Read in the image file as a data URL.
+      reader.readAsDataURL(f);
+    }
+  }
+
+  document.getElementById('files').addEventListener('change', handleFileSelect, false);
